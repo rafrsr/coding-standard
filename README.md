e1a24c7ac0f1f527f2bb4e936827435f877b310b
Rafael SR <raf.rsr@gmail.com>
2015-11-06 09:00:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# TopLib PHP CodeSniffer Coding Standard
+
+## Installation
+
+1. [Install composer](https://getcomposer.org/download/)
+2. Create a composer.json file who contain:
+
+````json
+{
+    "require-dev": {
+        "toplib/coding-standard": "dev-master"
+    }
+}
+````
+
+This project is licensed under the [MIT license](LICENSE).
---
+++ TopLib/Sniffs/NamingConventions/InterfaceSuffixSniff.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * This file is part of the Coding-Standard package.
+ *
+ * (c) RafaelSR <https://github.com/rafrsr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * TopLib_Sniffs_NamingConventions_InterfaceSuffixSniff.
+ *
+ * Throws errors if interface names are not suffixed with "Interface".
+ */
+class TopLib_Sniffs_NamingConventions_InterfaceSuffixSniff implements PHP_CodeSniffer_Sniff
+{
+    /**
+     * A list of tokenizers this sniff supports.
+     *
+     * @var array
+     */
+    public $supportedTokenizers = array(
+        'PHP',
+    );
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(T_INTERFACE);
+    }
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile All the tokens found in the document.
+     * @param int                  $stackPtr  The position of the current token in
+     *                                        the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens   = $phpcsFile->getTokens();
+        $line     = $tokens[$stackPtr]['line'];
+
+        while ($tokens[$stackPtr]['line'] == $line) {
+            if ('T_STRING' == $tokens[$stackPtr]['type']) {
+                if (substr($tokens[$stackPtr]['content'], -9) != 'Interface') {
+                    $phpcsFile->addError(
+                        'Interface name is not suffixed with "Interface"',
+                        $stackPtr
+                    );
+                }
+                break;
+            }
+            $stackPtr++;
+        }
+
+        return;
+    }
+}
